3af21f8b0bf9fc996474330bf3ef1e9388fcd046
Rohit Malusare <malusarerohit500@gmail.com>
2025-11-07 20:45:01 +0530

    Added getAllRecord class

---
+++ hibernate/hibernate_CRUD_operations/src/main/java/com/main/GetAllRecord.java
@@ -0,0 +1,38 @@
+package com.main;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import com.entity.Student;
+
+public class GetAllRecord {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Student.class);
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		Session ss = sf.openSession();
+		Transaction tr = ss.beginTransaction();
+		
+		//	SQL : Select * from student;  - table name as it is
+		//	HQL : from Student	- class name as table name, first letter capital
+		
+		String hqlQuery = "from Student";
+		
+		Query<Student> query = ss.createQuery(hqlQuery, Student.class);
+//		List<Student> list = query.getResultList();		-	old method
+		List<Student> list = query.list();
+		
+		
+		for(Student student : list) {
+			System.out.println(student);
+		}
+	}
+
+}
