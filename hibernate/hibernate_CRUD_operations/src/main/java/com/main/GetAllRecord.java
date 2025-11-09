07140bbd801803a0cf92410f125cca57d3113564
Rohit Malusare <malusarerohit500@gmail.com>
2025-11-09 13:56:36 +0530

    added HibernateCriteriaBuilder logic in GetAllRecord.java

--- hibernate/hibernate_CRUD_operations/src/main/java/com/main/GetAllRecord.java
+++ hibernate/hibernate_CRUD_operations/src/main/java/com/main/GetAllRecord.java
@@ -7,35 +7,49 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
+import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
 
 import com.entity.Student;
 
+import jakarta.persistence.criteria.Root;
+
 public class GetAllRecord {
-	
+
 	// here we get all records from the database using HQL
-	
+
 	public static void main(String[] args) {
 		Configuration cfg = new Configuration();
 		cfg.configure("hibernate.cfg.xml");
 		cfg.addAnnotatedClass(Student.class);
-		
+
 		SessionFactory sf = cfg.buildSessionFactory();
 		Session ss = sf.openSession();
 		Transaction tr = ss.beginTransaction();
-		
-		//	SQL : Select * from student;  - table name as it is
-		//	HQL : from Student	- class name as table name, first letter capital
-		
-		String hqlQuery = "from Student";
-		
-		Query<Student> query = ss.createQuery(hqlQuery, Student.class);
-//		List<Student> list = query.getResultList();		-	old method
+
+		// SQL : Select * from student; - table name as it is
+		// HQL : from Student - class name as table name, first letter capital
+
+//		String hqlQuery = "from Student";
+//		Query<Student> query = ss.createQuery(hqlQuery, Student.class);
+////		List<Student> list = query.getResultList();		-	old method
+//		List<Student> list = query.list();
+//		for(Student student : list) {
+//			System.out.println(student);
+//		}
+
+		// HibernateCriteriaBuilder is also use to execute select queries
+		HibernateCriteriaBuilder hcb = ss.getCriteriaBuilder();
+		JpaCriteriaQuery<Student> cq = hcb.createQuery(Student.class);
+		Root<Student> s = cq.from(Student.class);
+		cq.select(s);
+
+		Query<Student> query = ss.createQuery(cq);
 		List<Student> list = query.list();
-		
-		
 		for(Student student : list) {
-			System.out.println(student);
-		}
+		System.out.println(student);
+	}
+
 	}
 
 }
